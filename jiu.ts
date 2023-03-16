a5449e4b88ec8a8fde09bab751119114d4ce47ba
lxl <2312044132>
2023-03-17 00:08:38 +0800

    git 和 svn的区别

--- jiu.ts
+++ jiu.ts
@@ -1,14 +1,5 @@
-const MoveDecorator: ClassDecorator = (constructor: Function) => {
-  constructor.prototype.hd = '后盾人'
-  constructor.prototype.getPosition = (): { x: number; y: number } => {
-    return { x: 100, y: 100 }
-  }
-}
-
-@MoveDecorator
-class Tank {
-  constructor() {
-    console.log('tank 构造函d数')
-  }
-}
-const tank = new Tank()
+/**
+ * git 是一个分布式的代码托管平台，可以将代码完整的复制到每一台计算机上面，然后有开发人员进行修改 提交  推送  合并  等过程
+ * svn 是一个集中式的代码托管平台，代码集中放置在一台服务器上面，需要开发人员从中心服务器上拉取代码，然后修改合并提交等操作
+ * git 的分支管理能力比较强
+ */
